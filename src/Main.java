bc6689779195acc6c06b1d443a3775032b28a9d0
Sam.Donovan <samcdonovan@gmail.com>
2022-01-20 14:36:45 +0000

    Added csv data file reader

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * Main.java
+ * 
+ * @author Samuel C. Donovan
+ * 
+ * INSTRUCTIONS:
+*/
 public class Main {
 
 	public static void main(String[] args) {
-		
+
+	
+		String dataFile = System.getProperty("user.dir") + File.separator + "data" + File.separator + "cw2DataSet1.csv";
+		System.out.println("Loading from " + dataFile);
+
+		File file = new File(dataFile); /* create file object to use a scanner on */
+
+		try {
+			
+			Scanner scanner = new Scanner(file);
+			scanner.useDelimiter(","); /* delimit file by commas */   
+			while (scanner.hasNext())    
+			{  
+			System.out.print(scanner.next() + " ");   
+			}   
+
+		} catch (FileNotFoundException fileNotFound) { /* if file is not found, stop the program */
+			System.out.println("File not found at " + dataFile);
+			return;
+		} finally {
+
+		}
 
 	}
 
