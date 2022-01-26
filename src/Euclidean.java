bca71eede8f2fff6c11254096a560be96a321ecd
Sam.Donovan <samcdonovan@gmail.com>
2022-01-26 09:02:48 +0000

    Euclidean solution working, ~98.3% accurate

--- src/Euclidean.java
+++ src/Euclidean.java
@@ -1,4 +1,86 @@
+import java.util.List;
 
 public class Euclidean {
 
+	public static int categorise2(List<int[]> dataset1, List<int[]> dataset2) {
+		double min = Float.MAX_VALUE;
+		double currentDist;
+		int minPos = 0;
+		int numCorrect = 0;
+		int dataset1Size = dataset1.size();
+		int dataset2Size = dataset2.size();
+		int lastIndex = dataset1.get(0).length - 1;
+
+		for (int pos = 0; pos < dataset1Size; pos++) {
+
+			min = Float.MAX_VALUE;
+
+			for (int pos2 = 0; pos2 < dataset2Size; pos2++) {
+
+				currentDist = euclidean(dataset1.get(pos), dataset2.get(pos2));
+
+				if (currentDist < min) {
+					min = currentDist;
+					minPos = pos2;
+				}
+			}
+
+			System.out.println(pos + " : " + dataset1.get(pos)[lastIndex] + ", closest = " + minPos + " : "
+					+ dataset2.get(minPos)[lastIndex]);
+
+			if (dataset1.get(pos)[lastIndex] == dataset2.get(minPos)[lastIndex])
+				numCorrect++;
+		}
+
+		return numCorrect;
+	}
+
+	public static int categorise(int current, List<int[]> dataset) {
+		double min = Float.MAX_VALUE;
+		double currentDist;
+		int minPos = 0;
+		int numCorrect = 0;
+		int datasetSize = dataset.size();
+		int lastIndex = dataset.get(0).length - 1;
+
+		for (int pos = current * 10; pos < (current * 10) + 10 && pos < datasetSize; pos++) {
+
+			min = Float.MAX_VALUE;
+
+			for (int pos2 = 0; pos2 < datasetSize; pos2++) {
+
+				if (current * 10 == pos2) {
+					pos2 += 10;
+
+					if (pos2 >= datasetSize - 1)
+						continue;
+				}
+
+				currentDist = euclidean(dataset.get(pos), dataset.get(pos2));
+
+				if (currentDist < min) {
+					min = currentDist;
+					minPos = pos2;
+				}
+			}
+
+			System.out.println(pos + " : " + dataset.get(pos)[lastIndex] + ", closest = " + minPos + " : "
+					+ dataset.get(minPos)[lastIndex]);
+
+			if (dataset.get(pos)[lastIndex] == dataset.get(minPos)[lastIndex])
+				numCorrect++;
+		}
+
+		return numCorrect;
+	}
+
+	public static double euclidean(int[] digit1, int[] digit2) {
+
+		int sum = 0;
+
+		for (int pos = 0; pos < digit1.length - 1; pos++)
+			sum += ((digit1[pos] - digit2[pos]) * (digit1[pos] - digit2[pos]));
+
+		return Math.sqrt(sum);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Main.java
@@ -14,28 +17,73 @@ public class Main {
 
 	public static void main(String[] args) {
 
-	
-		String dataFile = System.getProperty("user.dir") + File.separator + "data" + File.separator + "cw2DataSet1.csv";
-		System.out.println("Loading from " + dataFile);
+		int totalCorrect = 0;
 
-		File file = new File(dataFile); /* create file object to use a scanner on */
+		String dataFile1 = System.getProperty("user.dir") + File.separator + "data" + File.separator
+				+ "cw2DataSet1.csv";
+		System.out.println("Loading from " + dataFile1);
+
+		String dataFile2 = System.getProperty("user.dir") + File.separator + "data" + File.separator
+				+ "cw2DataSet2.csv";
+		System.out.println("Loading from " + dataFile2);
+
+		ArrayList<int[]> dataset1 = readFile(dataFile1);
+		ArrayList<int[]> dataset2 = readFile(dataFile2);
+		int fullDatasetSize = dataset1.size() + dataset2.size();
+		totalCorrect += Euclidean.categorise2(dataset1, dataset2) + Euclidean.categorise2(dataset2, dataset1);
+
+		if (dataset1.size() > 0 && dataset2.size() > 0) {
+			/*for (int currentNum = 0; currentNum < data.size(); currentNum++) {
+				totalCorrect += Euclidean.categorise(currentNum, data);
+				System.out.println(totalCorrect);
+			}*/
+			System.out.println("Total correct: " + totalCorrect + "/" + fullDatasetSize + " = "
+					+ (((double) totalCorrect / (double) fullDatasetSize) * 100) + "%");
+		}
+	}
+
+	public static ArrayList<int[]> readFile(String filePath) {
+		File file = new File(filePath); /* create file object to use a scanner on */
+
+		String line = "", delimiter = ",";
+		String[] tempArray;
+		int[] array = new int[65];
+		ArrayList<int[]> list = new ArrayList<int[]>();
 
 		try {
-			
-			Scanner scanner = new Scanner(file);
-			scanner.useDelimiter(","); /* delimit file by commas */   
-			while (scanner.hasNext())    
-			{  
-			System.out.print(scanner.next() + " ");   
-			}   
 
-		} catch (FileNotFoundException fileNotFound) { /* if file is not found, stop the program */
-			System.out.println("File not found at " + dataFile);
-			return;
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferReader = new BufferedReader(fileReader);
+
+			while ((line = bufferReader.readLine()) != null) {
+				array = new int[65];
+				tempArray = line.split(delimiter);
+
+				for (int pos = 0; pos < tempArray.length; pos++) {
+					array[pos] = Integer.parseInt(tempArray[pos]);
+
+				}
+
+				list.add(array);
+			}
+
+			bufferReader.close();
+
+		} catch (IOException fileNotFound) { /* if file is not found, stop the program */
+			System.out.println("File not found at " + filePath);
+			return list;
 		} finally {
-
+			return list;
 		}
+	}
 
+	public static void print2D(List<int[]> list) {
+		for (int pos = 0; pos < list.size(); pos++) {
+			for (int pos2 = 0; pos2 < list.get(0).length; pos2++) {
+				System.out.print(list.get(pos)[pos2] + ", ");
+			}
+			System.out.println();
+		}
 	}
 
 }
