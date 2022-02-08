afe8e982f1eed37965f3a27da2624a27392e80a0
Sam.Donovan <samcdonovan@gmail.com>
2022-02-08 14:06:53 +0000

    GA revamped, ~50% accuracy

--- src/GeneticAlgorithm.java
+++ src/GeneticAlgorithm.java
@@ -10,74 +10,149 @@
  */
 public class GeneticAlgorithm {
 
-	private final int popSize = 20;
-	private final int geneLength = 2810;
-	private final int generations = 3;
-
-	int[][] population;
+	int[][] population; /* 2D array that holds the current population */
+	private final int populationSize = 40;
+	private final int geneLength = 640;
+	private final int generations = 300;
+	private final double mutationRate = 1.5;
 
 	/**
+	 * Function that runs the genetic algorithm with a 2-fold test
 	 * 
-	 * @param dataset1
-	 * @param dataset2
+	 * @param dataset1 (int[][]), the first dataset
+	 * @param dataset2 (int[][]), the second dataset
 	 */
 	public void run(int[][] dataset1, int[][] dataset2) {
 
-		generateInitialPopulation(dataset1);
-		
+		/* generate an initial population using dataset1 */
+		generatePopulation(dataset1);
+
 		for (int i = 0; i < generations; i++) {
-			printPopAndEval(dataset2);
+			
+			uniformCrossover(dataset1);
+
+		}
+
+		int firstFoldTotal = twoFold(dataset2);
+		System.out.println("Training set: cw2DataSet1.csv, test set: cw2DataSet2.csv");
+		System.out.println("Correct categorisations = " + firstFoldTotal + "/" + dataset2.length + "\n");
+
+		/* generate an initial population using dataset1 */
+		generatePopulation(dataset2);
+	
+		/* run the genetic algorithm for the specified number of generations */
+		for (int i = 0; i < generations; i++) {
+
 			uniformCrossover(dataset2);
 
 		}
+
+		int secondFoldTotal = twoFold(dataset1);
+		System.out.println("Training set: cw2DataSet2.csv, test set: cw2DataSet1.csv");
+		System.out.println("Correct categorisations = " + secondFoldTotal + "/" + dataset1.length);
+		int totalCorrect = firstFoldTotal + secondFoldTotal;
+
+		System.out.println("\nTotal correct: " + totalCorrect + "/" + (dataset1.length + dataset2.length) + " = "
+				+ ((double) totalCorrect / (double) (dataset1.length + dataset2.length)) * 100 + "%");
+
 	}
 
-	public void printPopAndEval(int[][] dataset) {
-
-		for (int i = 0; i < popSize; i++) {
-
-			System.out.print(i + " " + fitness(population[i], dataset));
-			System.out.println();
-		}
-	}
-
-	public int[] generateRandomGene() {
-
+	public int[] generateGene() {
 		int[] gene = new int[geneLength];
 
-		for (int genePos = 0; genePos < geneLength; genePos++)
-			gene[genePos] = (int) (Math.random() * 10);
+		for (int genePos = 0; genePos < gene.length; genePos++) {
+			gene[genePos] = (int) (Math.random() * 17);
+		}
 
 		return gene;
 	}
 
+	public void generatePopulation(int[][] dataset) {
+		population = new int[populationSize][geneLength];
+
+		for (int currentGene = 0; currentGene < populationSize; currentGene++)
+
+			population[currentGene] = generateGene();
+
+	}
+
+	public static int[] getRow(int[] gene, int category) {
+
+		int[] row = new int[64];
+		// System.out.println("cat" +category);
+		int currentSection = category * 64;
+
+		for (int genePos = currentSection, rowPos = 0; genePos < currentSection + row.length; genePos++, rowPos++) {
+			// System.out.println("rowgene"+genePos);
+			row[rowPos] = gene[genePos];
+		}
+		return row;
+	}
+
+	public int twoFold(int[][] dataset) {
+		int bestFitness = 0;
+		int currentFitness = 0;
+		for (int pos = 0; pos < population.length; pos++) {
+
+			currentFitness = euclideanFitness(population[pos], dataset);
+			if (currentFitness > bestFitness)
+				bestFitness = currentFitness;
+		}
+
+		return bestFitness;
+	}
+
+	public static int euclideanFitness(int[] gene, int[][] dataset) {
+
+		int numCorrect = 0;
+		double currentDist;
+		double min = Double.MAX_VALUE;
+		int minPos = -1;
+		int[] currentRow;
+
+		for (int pos = 0; pos < 10; pos++) {
+			// System.out.println("pos"+pos);
+			currentRow = getRow(gene, pos);
+			for (int datasetPos = 0; datasetPos < dataset.length; datasetPos += 10) {
+				for (int somethign = datasetPos; somethign < datasetPos + 10; somethign++) {
+					// System.out.println(datasetPos);
+					// System.out.println(somethign);
+					currentDist = euclideanDistance(currentRow, dataset[somethign]);
+
+					/* if the distance between the two rows from each dataset is smaller than the current
+					 * minimum distance, set minimum distance to this new distance and save the position in minPos*/
+					if (currentDist < min) {
+						min = currentDist;
+						minPos = somethign;
+					}
+				}
+
+				/* if the nearest neighbour both have the same category in their last cell (65)
+				 * the categorisation is correct, numCorrect is incremented by 1 */
+				if (pos == dataset[minPos][dataset[0].length - 1])
+					numCorrect++;
+
+			}
+		}
+		return numCorrect;
+	}
+
 	/**
-	 * Generates an initial population using the categories 
-	 * from the current dataset
-	 * 
-	 * @param dataset (int[][]), current dataset for 2-fold test
+	 * Euclidean distance calculator, calculates distance
+	 * between two rows of data from the dataset 
+	 * @param row1, int array representing a row in the dataset
+	 * @param row2, int array representing another row in the dataset
+	 * @return double, the Euclidean distance between each row
 	 */
-	public void generateInitialPopulation(int[][] dataset) {
+	public static double euclideanDistance(int[] row1, int[] row2) {
 
-		int[] datasetGene = new int[geneLength];
+		int sum = 0;
 
-		/* create a gene from the categories of each row in the current dataset */
-		for (int row = 0; row < datasetGene.length; row++)
-			datasetGene[row] = dataset[row][dataset[row].length - 1];
+		/* sums the distance between each point in both arrays */
+		for (int pos = 0; pos < row1.length - 1; pos++)
+			sum += ((row1[pos] - row2[pos]) * (row1[pos] - row2[pos]));
 
-		/* set the class variable "population" to a new 2D array */
-		this.population = new int[popSize][geneLength];
-
-		/* fill population with the gene created from the dataset */
-		for (int currentGene = 0; currentGene < popSize; currentGene++)
-			this.population[currentGene] = datasetGene.clone();
-/*
-		population[0] = datasetGene;
-		
-		for (int currentGene = 1; currentGene < popSize; currentGene++)
-			//this.population[currentGene] = datasetGene;
-			this.population[currentGene] = generateRandomGene();
-	*/	 
+		return Math.sqrt(sum);
 	}
 
 	/**
@@ -100,25 +175,10 @@ public class GeneticAlgorithm {
 		return numCorrect;
 	}
 
-	public void mutation(int[] gene) {
-
-		double mutationRate = 1.5;
-		double rand;
-
-		for (int pos = 0; pos < gene.length; pos++) {
-
-			rand = Math.random() * 100;
-
-			if (mutationRate >= rand)
-				gene[pos] = (int) (Math.random() * 10);
-
-		}
-	}
-
 	/**
 	 * Retrieves the two genes in the population with the highest fitness
 	 * 
-	 * @param dataset
+	 * @param dataset (int[][]), current dataset that is being trained on
 	 * @return
 	 */
 	public int[] findBestGenes(int[][] dataset) {
@@ -129,10 +189,10 @@ public class GeneticAlgorithm {
 		/* loop through each gene in the population in order 
 		 * to find the two genes with the highest fitnesses */
 		for (int currentGene = 0; currentGene < population.length; currentGene++) {
-			
+
 			/* get fitness for the current gene */
-			currentEval = fitness(population[currentGene], dataset);
-			System.out.println(currentGene + " : " + currentEval);
+			currentEval = euclideanFitness(population[currentGene], dataset);
+			// System.out.println(currentGene + " : " + currentEval);
 
 			if (currentEval > firstHighest) {
 				secondHighest = firstHighest;
@@ -149,47 +209,61 @@ public class GeneticAlgorithm {
 
 		/* return an array containing the positions of the two best genes in the population */
 		int[] highestArr = { firstHighestPos, secondHighestPos };
-		System.out.println("Best: " + firstHighestPos + " " + secondHighestPos);
+		// System.out.println("Best: " + firstHighestPos + " " +
+		// secondHighestPos);
 		return highestArr;
 	}
 
 	/**
-	 *
+	 * Uniform crossover function; for each category in the gene, there
+	 * is a 50% chance for it to crossover. If the category already matches
+	 * the category in the dataset, it does not crossover.
 	 * 
-	 * @param dataset
+	 * @param dataset (int[][]), current dataset that is being trained
 	 */
 	public void uniformCrossover(int[][] dataset) {
 		double crossoverChance = 50.0;
 		double randomChance = Math.random();
-
+		int tempElement;
 		int[] bestGenes = findBestGenes(dataset);
 
 		int[] gene1 = population[bestGenes[0]], gene2 = population[bestGenes[1]];
-		int[] newGene = gene1;
+		int[] newGene1 = gene1.clone(), newGene2 = gene2.clone();
 
-		for (int newGenePos = 0; newGenePos < popSize; newGenePos++) {
-		
-			newGene = gene1.clone();
+		/* create a new gene using uniform crossover for each position in the population */
+		for (int newGenePos = 0; newGenePos < populationSize; newGenePos++) {
 
+			newGene1 = gene1.clone();
+			newGene2 = gene2.clone();
+
+			/* loop through each bit in the gene with a 50% chance for a crossover to occur */
 			for (int pos = 0; pos < gene1.length; pos++) {
 
-				if (gene1[pos] != dataset[pos][64]) {
+				/* if the current category matches the category in the dataset (at the same position),
+				 * crossover and mutation are not performed. This helps ensure fitness remains high */
+				// if (gene1[pos] != dataset[pos][64]) {
 
-					randomChance = Math.random() * 100;
-					if (randomChance >= crossoverChance) {
+				randomChance = Math.random() * 100;
 
-						newGene[pos] = gene2[pos];
-
-					}
-
-					newGene[pos] = mutate(newGene[pos]);
+				/* if the random chance is higher than the crossover chance, perform crossover */
+				if (randomChance >= crossoverChance) {
+					tempElement = newGene1[pos];
+					newGene1[pos] = gene2[pos];
+					newGene2[pos] = tempElement;
 				}
+
+				/* call mutate on the current gene bit (category) */
+				newGene1[pos] = mutate(newGene1[pos]);
+				newGene2[pos] = mutate(newGene2[pos]);
+				// }
 			}
 
-			population[newGenePos] = newGene.clone();
+			/* insert the new gene into the population */
+			population[newGenePos] = newGene1.clone();
 
+			population[++newGenePos] = newGene2.clone();
 		}
-		//print2D(population);
+
 	}
 
 	/**
@@ -201,23 +275,14 @@ public class GeneticAlgorithm {
 	 */
 	public int mutate(int geneBit) {
 
-		double mutationRate = 1.5;
 		double rand = Math.random() * 100;
 
 		/* if the random number is less than or equal to the mutation rate,
 		 * the gene bit is mutated */
 		if (mutationRate >= rand)
-			geneBit = (int) (Math.random() * 10);
+			geneBit = (int) (Math.random() * 17);
 
 		return geneBit;
 	}
 
-	public static void print2D(int[][] list) {
-		for (int pos = 0; pos < list.length; pos++) {
-			for (int pos2 = 0; pos2 < list[0].length; pos2++) {
-				System.out.print(list[pos][pos2]);
-			}
-			System.out.println();
-		}
-	}
 }
